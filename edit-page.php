6c24f7ca500812fc378e0a12e1b3efd9eefea152
hilmantrihatmojo <mohammed.hilmantrihatmojo@btpn.com>
2023-01-30 21:48:36 +0700

    [hilman] handle edit page using form from other files

---
+++ edit-page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit Form</title>
+</head>
+
+<body>
+	<header>
+		<h3>Edit Account Form</h3>
+	</header>
+	
+    <?php include($_SERVER['DOCUMENT_ROOT'].'/php-exercise/form/edit-form.php'); ?>
+	</body>
+</html>
